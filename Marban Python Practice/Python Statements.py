c49d82d225a41e649f5bc33bbe7403fcddde80f7
Fernando Marban <fernandomarban@Fernandos-MacBook-Pro.local>
2022-12-16 20:03:37 -0600

    CODE-15: Marban Practice Folder Addition

--- Marban Python Practice/Python Statements.py
+++ Marban Python Practice/Python Statements.py
@@ -1,9 +1,9 @@
-x=19
-if x==10:
+x = 19
+if x == 10 :
     print("x is 10.")
-elif x%2==0:
+elif x % 2 == 0 :
     print("x is even")
-elif x==19:
+elif x == 19 :
     print("x is 19")
 else:
     print("x is not 10")
